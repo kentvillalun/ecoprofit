6d4b2acfcf7343cfbd2257a01fbf545c42652916
KentDoCode <villalunkent03@gmail.com>
2026-01-26 23:18:10 +0800

    Databse setup and error handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { config } from "dotenv"
+import { connectDB, disconnectDB } from "./config/db.js";
+
 
 import auth from "./routes/auth.route.js"
 
-
+config();
+connectDB();
 
 const app = express();
 
@@ -18,3 +22,28 @@ const server = app.listen(PORT, () => {
     console.log("Server running on " + PORT)
 })
 
+
+// Handle unhandled promise rejections (e.g., databse connection errors)
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection:", err);
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+// Handle uncaught exceptions 
+process.on("uncaughtException", async (err) => {
+    console.error("Uncaught Exceptions:", err);
+    await disconnectDB();
+    process.exit(1);
+});
+
+// Graceful shutdown 
+process.on("SIGTERM", async (err) => {
+    console.log("SIGTERM received, shutting down gracefully");
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
