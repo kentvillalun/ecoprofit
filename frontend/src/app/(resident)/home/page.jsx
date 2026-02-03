6bbc1681b62697b559d7123c483715f276266144
KentDoCode <villalunkent03@gmail.com>
2026-02-04 00:00:16 +0800

    started Resident home screen

--- frontend/src/app/(resident)/home/page.jsx
+++ frontend/src/app/(resident)/home/page.jsx
@@ -3,7 +3,8 @@
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 import { BellIcon } from "@heroicons/react/24/outline";
-
+import { CameraIcon } from "@heroicons/react/24/solid"
+ 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -16,15 +17,42 @@ export default function HomePage() {
     >
       <header className="flex flex-row items-start justify-between min-w-full max-h-18.75 bg-white  fixed top-0 p-5 shadow-lg">
         <div className="flex flex-row justify-between min-w-full ">
-          <Image src="/logo-solo.svg" width={32} height={38} alt="Logo"/>
-          <BellIcon className="w-7.25 h-7.25" /> 
+          <Image src="/logo-solo.svg" width={32} height={38} alt="Logo" />
+          <BellIcon className="w-7.25 h-7.25" />
         </div>
       </header>
 
-      <section className="relative top-[calc()]">
-        <p className="font-medium">Hi, Jaymar</p>
+      <section className="absolute top-18 max-h-[calc(100dvh-72px)] p-3 flex flex-col gap-6">
+
+        <div className="flex flex-col items-start">
+          <p className="font-medium text-[14px]">Hi, Jaymar</p>
+          <p className="text-[14px]">Sell your recyclables to Barangay Beddend Laud</p>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <div className="bg-[#89D957] min-h-45 min-w-45 flex flex-col items-center justify-start gap-1 pt-7 rounded-full border-2 border-white shadow-gray-400 shadow-lg">
+            <CameraIcon className="h-21 w-21 fill-white"/>
+            <p className="font-medium text-[14px] text-white">Capture Recyclables</p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-3">
+          <div className="bg-white p-2 rounded-[10px]">
+            <p className="font-medium text-[12px]">Barangay Schedule Today:</p>
+            <p className="text-[12px] text-[#727272]">Tue & Thu — 9:00 AM to 4:00 PM</p>
+          </div>
+          <div className="bg-white p-2 rounded-[10px]">
+            <p className="font-medium text-[12px]">Current Buying Prices:</p>
+            <p className="text-[12px] text-[#727272]">PET: ₱8/kg</p>
+            <p className="text-[12px] text-[#727272]">Cardboard: ₱5/kg</p>
+          </div>
+        </div>
+
+        <div className="">
+          <p className="font-medium text-[12px]">Recent Requests</p>
+          
+        </div>
       </section>
     </main>
   );
 }
-       
--- frontend/src/components/navigation/ResidentBottomNav.jsx
+++ frontend/src/components/navigation/ResidentBottomNav.jsx
@@ -14,28 +14,34 @@ const poppins = Poppins({
 
 export const ResidentBottomNav = () => {
   return (
-    <section className="min-h-18.75 absolute bottom-0 bg-white min-w-screen pb-2.25 flex flex-row items-center justify-center px-7.75">
-      <div className="grid grid-cols-5 gap-1 text-[#9DB2CE]">
-        <div className="flex flex-col items-center justify-center">
-          <HomeIcon className="h-6.25 w-6.25" />
-          <p className="text-[12px]">Home</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <MapPinIcon className="h-6.25 w-6.25" />
-          <p className="text-[12px]">Barangay</p>
-        </div>
-
-        <div className="min-w-16 min-h-16 bg-[#89D957] flex items-center justify-center rounded-full border-2 border-white shadow-gray-400 shadow-lg trasform -translate-y-10">
-          <CameraIcon className="fill-white h-7.75  w-7.75" />
+    <section className="min-h-18.75 absolute bottom-0 bg-white min-w-screen pb-2.25 flex flex-row items-center justify-center px-3">
+      <div className="grid grid-cols-3 gap-1 text-[#9DB2CE]">
+        <div className="grid grid-cols-2 gap-3">
+          <div className="flex flex-col items-center justify-center">
+            <HomeIcon className="h-6.25 w-6.25" />
+            <p className="text-[12px]">Home</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <MapPinIcon className="h-6.25 w-6.25" />
+            <p className="text-[12px]">Barangay</p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center">
-          <ClipboardIcon className="h-6.25 w-6.25" />
-          <p className="text-[12px]">Requests</p>
+        <div className="">
+          <div className="min-w-16 min-h-16 bg-[#89D957] flex items-center justify-center rounded-full border-2 border-white shadow-gray-400 shadow-lg trasform -translate-y-10">
+            <CameraIcon className="fill-white h-7.75  w-7.75" />
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <UserIcon className="h-6.25 w-6.25" />
-          <p className="text-[12px]">Profile</p>
+
+        <div className="grid grid-cols-2 gap-3">
+          <div className="flex flex-col items-center justify-center">
+            <ClipboardIcon className="h-6.25 w-6.25" />
+            <p className="text-[12px]">Requests</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <UserIcon className="h-6.25 w-6.25" />
+            <p className="text-[12px]">Profile</p>
+          </div>
         </div>
       </div>
     </section>
