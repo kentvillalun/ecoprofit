5c5dea87035d7e72c3649b1f93d4212f372d08b0
KentDoCode <villalunkent03@gmail.com>
2026-04-03 19:31:18 +0800

    change cors and variables

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,20 @@
 const nextConfig = {
   /* config options here */
   allowedDevOrigins: ["192.168.1.30"],
+  async rewrites() {
+    const backendUrl = process.env.NODE_ENV === "development" ? "http://localhost:5001" : "https://ecoprofit-production.up.railway.app";
+
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendUrl}/:path*`
+      }
+    ]
+  }
 };
+
+
 
 
 
